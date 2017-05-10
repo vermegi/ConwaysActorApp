da3c383b8339b654c7bfdd030664370ed17136c4
Gitte Vermeiren <gitte.vermeiren@gmail.com>
2017-05-10 15:40:17 +0200

    initiate the game actor

---
+++ src/ConwaysApi/Controllers/GameController.cs
@@ -0,0 +1,20 @@
+﻿using ConwaysApi.Domain;
+using System.Web.Http;
+
+namespace ConwaysApi.Controllers
+{
+    [ServiceRequestActionFilter]
+    class GameController : ApiController 
+    {
+        public string Get()
+        {
+            return "the game";
+        }
+
+        public void Post([FromBody]string gameName)
+        {
+            var gameActor = new GameActorOrchestrator(gameName);
+            gameActor.Initiate();
+        }
+    }
+}
---
+++ src/ConwaysApi/Domain/GameActor.cs
@@ -0,0 +1,24 @@
+﻿using GameActor.Interfaces;
+using Microsoft.ServiceFabric.Actors;
+using Microsoft.ServiceFabric.Actors.Client;
+using System;
+
+namespace ConwaysApi.Domain
+{
+    class GameActorOrchestrator
+    {
+        private string gameName;
+
+        public GameActorOrchestrator(string gameName)
+        {
+            this.gameName = gameName;
+        }
+
+        internal async void Initiate()
+        {
+            var actorId = ActorId.CreateRandom();
+            var gameActor = ActorProxy.Create<IGameActor>(actorId, new Uri("fabric:/ConwaysActorApp/GameActor"));
+            await gameActor.Initiate();
+        }
+    }
+}
